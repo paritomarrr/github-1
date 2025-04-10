e20f752ade60c048b6ea90cdf68802a7ea29110c
paritomar <pari@nes.tech>
2025-04-10 19:49:13 +0530

    Added files

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const simpleGit = require('simple-git');
 
 const FILE_PATH = './data.json';
 
-const DATE = moment().format();
+const DATE = moment().subtract(1, 'd').format();
 
 
 const data = {
@@ -16,4 +16,4 @@ jsonfile.writeFile(FILE_PATH, data)
 
 simpleGit().add([FILE_PATH]).commit(DATE, {
     '--date': DATE,
-})
+}).push()
